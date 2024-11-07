ce94aafc11d481650b4855a6845482fc861ee867
Jay Nielsen <robots@nlsn.xyz>
2024-11-07 21:57:53 +0000

    fix: reverse AoY post layout

--- src/app/[slug]/components/album-post.tsx
+++ src/app/[slug]/components/album-post.tsx
@@ -15,20 +15,24 @@ export const AlbumPost = ({ post }: PostProps) => {
       {post.description && (
         <Text fontSize="var(--gaze-font-size-2xl)">{post.description}</Text>
       )}
-      <Switcher limit={2} space="var(--gaze-space-50)" threshold="90rem">
-        <Box borderRadius="var(--gaze-radius-sm)">
-          <iframe
-            src={`https://embed.tidal.com/albums/${post.tidalId}?disableAnalytics=true`}
-            allow="encrypted-media"
-            allowFullScreen
-            frameBorder="0"
-            className={styles["tidal-iframe"]}
-          ></iframe>
-        </Box>
+      <Switcher
+        limit={2}
+        space="var(--gaze-space-50)"
+        threshold="90rem"
+        align="stretch"
+        reverse
+      >
         <Box
           className={styles["md"]}
           dangerouslySetInnerHTML={{ __html: post.content ?? "" }}
         />
+        <iframe
+          src={`https://embed.tidal.com/albums/${post.tidalId}?disableAnalytics=true`}
+          allow="encrypted-media"
+          allowFullScreen
+          frameBorder="0"
+          className={styles["tidal-iframe"]}
+        ></iframe>
       </Switcher>
     </Stack>
   );
